6aefd6dd7243f26dda81c6d37dfd1c3d0a8607f6
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-01-10 16:17:20 +0100

    Test check frase

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("asd")
+const core = require('@actions/core');
+const frase_buena = core.getInput('frase_buena');
+const frase_negativa = core.getInput('frase_negativa');
+const resultado_tests = core.getInput('resultado_tests');
+
+try {
+    console.log(frase_buena)
+    console.log(frase_negativa)
+    console.log(resultado_tests)
+} catch (error) {
+    
+}
